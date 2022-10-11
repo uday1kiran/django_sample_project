c7f341a47b26ecb619d02908dee3618af3611b71
uday <uday.kiran.reddy@quest.com>
2022-10-11 23:09:21 +0530

    Added view to display records of a model/table

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from .models import Book
 
 # Create your views here.
 def first(request):
@@ -10,5 +11,14 @@ def firstfunction(request):
     return HttpResponse('First message from Views')
 
 class Another(View):
+    books = Book.objects.all()  ## to display all objects of that model.
+    ## books = Book.objects.filter(is_published=True)
+    ## books = Book.objects.get(is_published=True) ## filter will get all objects with that field value, but get will being only one object
+    ## book = Book.objects.get(id=2)
+    #output = f"We have {len(books)} books in DB"
+    output = ''
+    for book in books:
+        output += f"We have {book.title} book in DB with ID: {book.id}<br>"
+
     def get(self, request):
-        return HttpResponse('This is another function inside class')
+        return HttpResponse(self.output)
