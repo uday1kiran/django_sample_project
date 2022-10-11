036e137abb9003eb3a6ba7a10b3fb3276f9f6d50
uday <uday.kiran.reddy@quest.com>
2022-10-11 22:47:55 +0530

    - added dew more fields to model in demo app - created both class and method views - created urls.py to demo app and configured routes to that

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
- title = models.CharField(max_length=36)
+ '''STATUSES = (
+  (0,'Unknown'),
+  (1,'processed'),
+  (1, 'paid')
+ )'''
+ ## title = models.CharField(blank=False, null=True, unique=True, default='', choices=STATUSES) ## if blank is False, null won't be true anyway
+ title = models.CharField(max_length=36, blank=False, unique=True)
+ description = models.TextField(max_length=256,blank=True)
+ price = models.DecimalField(default=0, decimal_places=2, max_digits=3)
+ published = models.DateField(blank=True, null=True)
+ is_published = models.BooleanField(default=False)
+ cover=models.ImageField(upload_to='covers/', blank=True)  ## pip install Pillow , to support image uplaods
+
+ ## more details about fields and field types
+ # https://docs.djangoproject.com/en/4.1/ref/models/fields/
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+def first(request):
+    return HttpResponse('First message from Views')
+
+def firstfunction(request):
+    return HttpResponse('First message from Views')
+
+class Another(View):
+    def get(self, request):
+        return HttpResponse('This is another function inside class')
